b74d616dfb64115d59109dc4a9dee5b72ea7b593
pacodemant <148328057+pacodemant@users.noreply.github.com>
2026-06-25 10:59:32 +0200

    Run 281a: Versionsstring auf r281 aktualisiert

--- lib/pages/startmenue_seite.dart
+++ lib/pages/startmenue_seite.dart
@@ -125,7 +125,7 @@ class StartmenueSeite extends StatelessWidget {
                 const Spacer(),
                 const Center(
                   child: Text(
-                    'Web App 0.11.1 · r279e @ GitHub:',
+                    'Web App 0.11.1 · r281 @ GitHub:',
                     style: TextStyle(fontSize: 13, color: AppFarben.subtilerText),
                   ),
                 ),
