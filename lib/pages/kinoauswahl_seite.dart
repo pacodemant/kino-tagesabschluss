49d9595e93040e6ae953652c0cef94377b613ba9
pacodemant <148328057+pacodemant@users.noreply.github.com>
2026-06-26 11:11:07 +0200

    Run 291a2: Versionsstring auf r291a korrigiert

--- lib/pages/kinoauswahl_seite.dart
+++ lib/pages/kinoauswahl_seite.dart
@@ -65,7 +65,7 @@ class _KinoauswahlSeiteState extends State<KinoauswahlSeite> {
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   const Text(
-                    'Web App 0.9.1 · r291 @ GitHub:',
+                    'Web App 0.9.1 · r291a @ GitHub:',
                     style: TextStyle(
                       fontSize: 13,
                       color: AppFarben.subtilerText,
--- lib/pages/startmenue_seite.dart
+++ lib/pages/startmenue_seite.dart
@@ -125,7 +125,7 @@ class StartmenueSeite extends StatelessWidget {
                 const Spacer(),
                 const Center(
                   child: Text(
-                    'Web App 0.9.1 · r291 @ GitHub:',
+                    'Web App 0.9.1 · r291a @ GitHub:',
                     style: TextStyle(fontSize: 13, color: AppFarben.subtilerText),
                   ),
                 ),
